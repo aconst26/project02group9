f380347bddc8556342625f1df3914240dd60954a
Anthony Constante <85783762+aconst26@users.noreply.github.com>
2025-09-29 23:46:05 -0700

    comment

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -30,7 +30,7 @@ export default function LoginScreen() {
     setLoading(true);
   
     try {
-      const response = await fetch(`http://192.168.0.31:8080/users/login`, {
+      const response = await fetch(`http://192.168.0.31:8080/users/login`, { // points to back end so back end has to fetch from database ip might be hardcoded?
         method: "POST", // SENDING TO BACKEND / POST MAPPING
         headers: {
           "Content-Type": "application/json",
